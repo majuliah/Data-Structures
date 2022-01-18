e5136e648622e6eae6adceef62c58a3ee61a3f4b
Majulia <majulialcarvalho@gmail.com>
2022-01-18 20:22:46 -0300

    Primeiro exemplo de Pilha

---
+++ Stack/stack1.1/Pilha.cs
@@ -0,0 +1,37 @@
+﻿namespace stack1._1
+{
+    public class Pilha
+    {
+        private int valores[];
+        private int topo;
+
+        public Pilha()
+        {
+            valores = new int[10];
+            topo = -1;
+        }
+
+        public void push(int elemento)
+        {
+            topo++;
+            valores[topo] = elemento;
+        }
+
+        public bool isEmpty()
+        {
+            return (topo == -1);
+        }
+
+        public bool isFull()
+        {
+            return (topo == 9);
+        }
+
+        public int pop()
+        {
+            int element = valores[topo];
+            topo--;
+            return element;
+        }
+    }
+}
---
+++ Stack/stack1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stack1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
